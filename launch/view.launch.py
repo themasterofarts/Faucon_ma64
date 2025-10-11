40fe343e644a065ea73ac6d9bbfad8db6492b906
Klein237 <kleinfy51@gmail.com>
2025-10-12 00:38:26 +0200

    feat: add ros2 control

--- launch/view.launch.py
+++ launch/view.launch.py
@@ -2,10 +2,11 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os, xacro
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.conditions import IfCondition
+
 
 
 def generate_launch_description():
@@ -14,6 +15,12 @@ def generate_launch_description():
     pkg_maize_field = get_package_share_directory("virtual_maize_field")
     xacro_file = os.path.join(pkg_path, "description", "robot.urdf.xacro")
     robot_desc = xacro.process_file(xacro_file).toxml()
+
+    twist_mux_params = os.path.join(get_package_share_directory('base_desc'),'config','twrist_mux.yaml')
+
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -96,16 +103,50 @@ def generate_launch_description():
         arguments=["/camera/image_raw"],
     )
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+        condition=IfCondition(LaunchConfiguration('use_ros2_control'))
+    )
+
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+        condition=IfCondition(LaunchConfiguration('use_ros2_control'))
+    )
+
+   
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': use_sim_time, 'use_stamped': use_ros2_control}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+  
+
     return LaunchDescription(
         [
+            DeclareLaunchArgument('use_sim_time', default_value='true',
+                              description='Use sim time if true'),
+            DeclareLaunchArgument('use_ros2_control', default_value='true',
+                              description='ROS2 control enabled if true'),
+
             robot_state_publisher,
-            # joint_state_publisher_gui,
-            joint_state_publisher_node,
+            joint_state_publisher_gui,
+            #joint_state_publisher_node,
             rviz,
             spawn_robot,
             gz_bridge,
             gz_sim,
             #launch_world,
             ros_gz_image_bridge,
+
+            diff_drive_spawner,
+            joint_broad_spawner,
+            twist_mux
+            
         ]
     )
