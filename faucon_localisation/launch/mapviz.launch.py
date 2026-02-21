c7034ab8f6705bbd5d017637565414bcdc59a893
Klein237 <kleinfy51@gmail.com>
2026-02-21 15:58:21 +0100

    feat: add mapviz

---
+++ faucon_localisation/launch/mapviz.launch.py
@@ -0,0 +1,43 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+import os
+from ament_index_python.packages import get_package_share_directory
+
+gps_wpf_dir = get_package_share_directory("faucon_localisation")
+mapviz_config_file = os.path.join(gps_wpf_dir, "config", "mapviz_osm.mvc")
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package="mapviz",
+            executable="mapviz",
+            name="mapviz",
+            parameters=[{"config": mapviz_config_file}]
+        ),
+        launch_ros.actions.Node(
+            package="swri_transform_util",
+            executable="initialize_origin.py",
+            name="initialize_origin",
+            remappings=[
+                ("fix", "gnss/fix"),
+            ],
+        ),
+        launch_ros.actions.Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="swri_transform",
+            arguments=[
+                '--x', '0',
+                '--y', '0',
+                '--z', '0',
+                '--roll', '0',
+                '--pitch', '0',
+                '--yaw', '0',
+                '--frame-id', 'map',
+                '--child-frame-id', 'origin'
+            ]
+        )
+    ])
